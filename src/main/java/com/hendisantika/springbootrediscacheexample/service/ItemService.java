022e072ee35c4534132e873004b72ab7b31dea14
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-31 11:38:52 +0700

    #7 - Add getItem in ItemService

--- src/main/java/com/hendisantika/springbootrediscacheexample/service/ItemService.java
+++ src/main/java/com/hendisantika/springbootrediscacheexample/service/ItemService.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootrediscacheexample.service;
 import com.hendisantika.springbootrediscacheexample.entity.Item;
 import com.hendisantika.springbootrediscacheexample.repository.ItemRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,5 +28,12 @@ public class ItemService {
 
     public List<Item> items() {
         return itemRepository.findAll();
+    }
+
+    @Cacheable(value = "items", key = "#id")
+    public Item getItem(Integer id) {
+        Item item = itemRepository.findById(id).orElseThrow(RuntimeException::new);
+        log.info("Loading data from DB {}", item);
+        return item;
     }
 }
