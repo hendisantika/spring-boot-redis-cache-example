486b109d3be64ff74df4ead964b8a549825dbe12
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-31 11:26:30 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootrediscacheexample/SpringBootRedisCacheExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrediscacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisCacheExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisCacheExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootrediscacheexample/SpringBootRedisCacheExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrediscacheexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisCacheExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
