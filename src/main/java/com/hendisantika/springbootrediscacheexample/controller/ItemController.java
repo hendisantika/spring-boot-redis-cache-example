f7cff27e2674f059b20cb6c01b2aa0c19e746bb0
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-31 11:49:19 +0700

    #13 - Add getAllItems & updateItem API in ItemController

--- src/main/java/com/hendisantika/springbootrediscacheexample/controller/ItemController.java
+++ src/main/java/com/hendisantika/springbootrediscacheexample/controller/ItemController.java
@@ -5,9 +5,12 @@ import com.hendisantika.springbootrediscacheexample.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,13 +31,23 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+    @GetMapping
+    public List<Item> getAllItems() {
+        return itemService.items();
+    }
+
     @GetMapping("/{id}")
     public Item getItem(@PathVariable Integer id) {
         return itemService.getItem(id);
     }
 
-    @PostMapping("/")
+    @PostMapping
     public Item createItem(@RequestBody Item request) {
         return itemService.createItem(request);
+    }
+
+    @PutMapping("/{id}")
+    public Item updateItem(@PathVariable Integer id, @RequestBody Item request) {
+        return itemService.updateItem(id, request);
     }
 }
